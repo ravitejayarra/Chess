cd964b09382f36046b13f1b1a302cc12c7ae3207
Yarra Raviteja <39350907+ravitejayarra@users.noreply.github.com>
2023-06-20 23:20:39 +0530

    added config file for jwt token (#6)
    * added config file for jwt token
    
    * added plan

--- user-auth-service/Utils/utility.js
+++ user-auth-service/Utils/utility.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const config = require('config')
 
 exports.getToken = (user)=>{
-    const token = jwt.sign({ _id: user._id }, 'secretKey');
+    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
     return token;
 }
